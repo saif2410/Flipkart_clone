7357f530fff507a08bcf7002c02fa3b240380022
Rza-irfan <irfanrza.2012@gmail.com>
2021-05-21 15:33:04 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React asasass
+          Learn React asasasssdsded
         </a>
       </header>
     </div>
