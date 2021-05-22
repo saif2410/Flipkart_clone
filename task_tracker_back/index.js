6cb2732f6c153719768b1b12910afe2d57e4a451
Saif Ali Khan <saifalikhan2410@gmail.com>
2021-05-22 15:57:56 +0530

    purani glti reset kri

--- task_tracker_back/index.js
+++ task_tracker_back/index.js
@@ -4,17 +4,16 @@ const path = require('path');
 const fs = require('fs');
 const Task = require('./models/task');
 
-const PORT = 5000;
-
 const dbUrl='mongodb+srv://tester:tester123@ClusterName.m7tde.mongodb.net/testing?retryWrites=true&w=majority';
 mongoose.connect(dbUrl,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen( PORT, () => console.log( `Server running on port ${PORT}` )) ) 
+.then((result) => app.listen(3000)) 
 .catch((err) => console.log(err));
 
 const app = express();
 app.use(express.static( path.join( __dirname, 'public')));
 
-
+const PORT = 5000;
+app.listen( PORT, () => console.log( `Server running on port ${PORT}` ));
 
 app.get('/addtask',(req,res)=>{
     const task = new Task({
@@ -34,7 +33,7 @@ app.get('/addtask',(req,res)=>{
 app.get('/alltasks',(req,res)=>{
     Task.find()
     .then((result)=>{
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
         res.send(result);
     })
     .catch((err)=>{
